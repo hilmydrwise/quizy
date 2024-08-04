27818e9f8d249e64645ecedf58cc2c49aad26990
hilmydrwise <hilmyddr@gmail.com>
2024-08-04 14:52:57 +0700

    first commit

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa nama teknologi yang memungkinkan pengisian daya perangkat elektronik secara nirkabel dalam jarak beberapa meter?",
+        answer: "C. Wi-Charge",
+        options: [
+            "A. NFC Charging",
+            "B. Wireless Inductive Charging",
+            "C. Wi-Charge",
+            "D. Bluetooth Charging"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Teknologi jaringan seluler generasi kelima yang menawarkan kecepatan internet lebih tinggi disebut sebagai apa?",
+        answer: "C. 5G",
+        options: [
+            "A. 3G",
+            "B. 4G",
+            "C. 5G",
+            "D. LTE"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Apa nama alat yang menggunakan teknologi AI untuk menghasilkan teks yang sangat mirip dengan tulisan manusia?",
+        answer: "A. GPT-3",
+        options: [
+            "A. GPT-3",
+            "B. TensorFlow",
+            "C. Siri",
+            "D. DeepMind"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Teknologi apa yang memungkinkan perangkat rumah tangga seperti lampu dan termostat untuk saling berkomunikasi dan dikendalikan melalui internet?",
+        answer: "A. IoT (Internet of Things)",
+        options: [
+            "A. IoT (Internet of Things)",
+            "B. NFC (Near Field Communication)",
+            "C. RFID (Radio Frequency Identification)",
+            "D. VPN (Virtual Private Network)"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Apa nama teknologi yang menggunakan blockchain untuk mendesentralisasikan transaksi keuangan dan menghindari perantara seperti bank?",
+        answer: "B. Cryptocurrency",
+        options: [
+            "A. PayPal",
+            "B. Cryptocurrency",
+            "C. Fintech",
+            "D. E-commerce"
+        ]
+    },
+]
